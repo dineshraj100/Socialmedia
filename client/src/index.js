f2a7f62e971ca25c413b5622abdc7b0c35f51ac5
dineshraj100 <dineshrajmech01@gmail.com>
2023-03-20 02:56:27 +0530

    some minor bugs fixed

--- client/src/index.js
+++ client/src/index.js
@@ -17,10 +17,9 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
-import persistReducer from 'redux-persist/es/persistReducer';
 
 const persistConfig = { key: 'root', storage, version: 1 };
-const persistReducer = persistReducer(persistConfig, authReducer);
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
